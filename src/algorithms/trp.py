a2af3715ef09b2fc4022466ae4377595ee31215c
Rebel-Uranus <2967147716@qq.com>
2025-01-30 23:00:18 +0800

    Add files via upload

--- src/algorithms/trp.py
+++ src/algorithms/trp.py
@@ -49,6 +49,7 @@ class TRP(SAC):
 		self.reg_token = RegToken(args.projection_dim)
 		
 		self.predictor = nn.Sequential(
+			# nn.LayerNorm(args.projection_dim),
 			nn.Linear(args.projection_dim, args.projection_dim),
 			nn.ReLU(),
 			nn.Linear(args.projection_dim, args.projection_dim),
@@ -71,38 +72,6 @@ class TRP(SAC):
 			self.transformer.train(training)
 			self.predictor.train(training)
 
-	def update_critic(self, obs, action, reward, next_obs, not_done, L=None, step=None):
-		with torch.no_grad():
-			_, policy_action, log_pi, _ = self.actor(next_obs)
-			target_Q1, target_Q2 = self.critic_target(next_obs, policy_action)
-			target_V = torch.min(target_Q1,
-								 target_Q2) - self.alpha.detach() * log_pi
-			target_Q = reward + (not_done * self.discount * target_V)
-
-		if self.svea_alpha == self.svea_beta:
-			obs = utils.cat(obs, augmentations.random_overlay(obs.clone()))
-			action = utils.cat(action, action)
-			target_Q = utils.cat(target_Q, target_Q)
-
-			current_Q1, current_Q2 = self.critic(obs, action)
-			critic_loss = (self.svea_alpha + self.svea_beta) * \
-				(F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q))
-		else:
-			current_Q1, current_Q2 = self.critic(obs, action)
-			critic_loss = self.svea_alpha * \
-				(F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q))
-
-			obs_aug = augmentations.random_overlay(obs.clone())
-			current_Q1_aug, current_Q2_aug = self.critic(obs_aug, action)
-			critic_loss += self.svea_beta * \
-				(F.mse_loss(current_Q1_aug, target_Q) + F.mse_loss(current_Q2_aug, target_Q))
-
-		if L is not None:
-			L.log('train_critic/loss', critic_loss, step)
-			
-		self.critic_optimizer.zero_grad()
-		critic_loss.backward()
-		self.critic_optimizer.step()
 
 
 	def compute_segment_return_predict_loss(self, obs, action):
